9ac468f9a7d7363afa025deb5e9e12e24142a9a8
ayush chaurasia <ayush@delimp.com>
2023-08-08 13:48:42 +0530

    show_existing_data

--- dataController/dataController.js
+++ dataController/dataController.js
@@ -7,21 +7,21 @@ const cors = require('cors');
 
 exports.upload = async (req, res) => {
   const file = req.file;
-  const filename = file.originalname; 
+  const filename = file.originalname;
   try {
     // Check if the file with the same name exists in the collection
-    const existingFile = await DataModel.findOne({ filename });
-    if (existingFile) {
-      // File already exists, send a response indicating that the file exists
-      console.log('File already exists:', filename);
-      res.status(200).json({ exists: true });
+    const existingFiles = await DataModel.find({ filename });
+    if (existingFiles.length > 0) {
+      // Files with the given filename exist, send their data in the response
+       console.log('File already exists:', filename);
+      res.status(200).json({exists: true, Data: existingFiles });
 
     } else {
       // File doesn't exist, proceed with saving the data to MongoDB
       const results = [];
       fs.createReadStream(file.path)
-        .pipe(csvParser({ skipLines: 8 })) 
-        .on('data', (row) => {  
+        .pipe(csvParser({ skipLines: 8 }))
+        .on('data', (row) => {
           console.log('Row data:', row);
           results.push(row);
         })
@@ -76,18 +76,19 @@ exports.check = async (req, res) => {
 
   try {
     // Check if the file with the same name exists in the collection
-    const existingFile = await DataModel.findOne({ filename });
+    const existingFile = await DataModel.find({ filename });
 
-    if (existingFile) {
-      // File already exists, send a response indicating that the file exists
-      res.status(200).json({ exists: true });
+
+    if (existingFile.length > 0) {
+      // Files with the given filename exist, send their data in the response
+      res.status(200).json({message: 'Data exist already', exists: true, Data: existingFile });
     } else {
-      // File does not exist, send a response indicating that the file does not exist
+      // No files with the given filename exist, send a response indicating that the files do not exist
       res.status(200).json({ exists: false });
     }
   } catch (err) {
     console.error('Error checking file existence:', err);
-    res.status(500).json({ error: 'Failed to check file existence' });
+    res.status(500).json({ error: 'Failed to check file existence', details: err.message });
   }
 };
 
